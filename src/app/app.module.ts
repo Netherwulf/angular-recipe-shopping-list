c5469c4a44ca644de5f9da27cd8ddddb38c581ab
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-30 15:24:43 +0200

    Added retrieving params from route and routes for new and edit recipe
    Added retrieving params from route and routes for new and edit recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import { NoRecipeSelectedComponent } from './recipes/no-recipe-selected/no-recipe-selected.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { NoRecipeSelectedComponent } from './recipes/no-recipe-selected/no-recip
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    NoRecipeSelectedComponent
+    NoRecipeSelectedComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
