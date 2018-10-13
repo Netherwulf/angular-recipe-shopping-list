6bec0f00b8908bd5984a1c4f3917f51edd66df5e
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-10-13 16:29:17 +0200

    Added Firebase Setup
    Added Firebase Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = '';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA3dhYrhWxBIvmYuh8KHY9FGQ4VfkyQSko',
+      authDomain: 'ng-recipe-book-86501.firebaseapp.com'
+    });
   }
 }
